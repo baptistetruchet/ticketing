099e92629e9827ca1f2fb002fce51afc72c60a0e
Baptiste Truchet <baptiste.truchet@gmail.com>
2020-05-14 16:56:50 +0200

    end test routes

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signIn(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -27,3 +36,15 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signIn = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/sign-up")
+    .send({ email, password })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
